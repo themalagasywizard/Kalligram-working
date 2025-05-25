e48b532fb27e2cee32cfb5061f567ae3d9c060e3
themalagasywizard <ryantrumble1997@gmail.com>
2025-05-26 01:12:32 +0200

    Clean up old HTML files and finalize TypeScript migration - Deleted old HTML files (index.html, app.html, app-typescript.html, context.html, profile.html, waitlist.html) - Fixed postbuild script for Windows compatibility - Cleaned up next.config.js - Verified TypeScript app is properly using shadcn components and types - Auth files are properly copied to out directory for deployment

--- Desktop/aiwritingtool-landingpage/.next/server/middleware-build-manifest.js
+++ Desktop/aiwritingtool-landingpage/.next/server/middleware-build-manifest.js
@@ -1,18 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-a131ddbedfadf161.js","static/chunks/fd9d1056-764cda20c6e1258e.js","static/chunks/117-b91cd82c16179e24.js","static/chunks/main-app-98c5f8d31ea3dcc8.js"],pages:{"/_app":["static/chunks/webpack-a131ddbedfadf161.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ef81d9adb47d1266.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-a131ddbedfadf161.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ef81d9adb47d1266.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
